f6a1beaa96ab6b1d1329892f97678d58bce5e6e5
Marcelo Menegheti <marcelomenegheti18@gmail.com>
2024-02-05 22:11:10 -0300

    git commit

--- js/script.js
+++ js/script.js
@@ -8,12 +8,12 @@ let menu = document.querySelector('#menu-icon');
 let navlist = document.querySelector('.navlist');
 
 menu.onclick = () => {
-    menu.classList.toggle('bx-x');
+    menu.classList.toggle('bx bx-menu');
     navlist.classList.toggle('active');
 
 };
 window.onscroll = () => {
-    menu.classList.remove('bx-x');
+    menu.classList.remove('bx bx-menu');
     navlist.classList.remove('active');
 };
 
